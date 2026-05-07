760a287a26f3d0be086fd5372f8f56c23a392e49
hafeez-star <hafeezullah4217@gmail.com>
2026-05-07 11:06:41 +0500

    Right-jsx

--- src/component/Right.jsx
+++ src/component/Right.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 const Right = () => {
     return (
         <div className='gird grid-cols-2  '>
-            <div className=' bg-white rounded-2xl shadow-2xl p-4  relative'>
+            <div className=' bg-white rounded-2xl shadow-2xl p-4  relative '>
                 <h1 className='text-1xl font-bold'>Air conditioning</h1>
                 <h1 className='text-violet-500 font-bold text-3xl'>17°C</h1>
                 <p className='text-gray-400'>Celsius</p>
@@ -19,8 +19,18 @@ const Right = () => {
 
                 </div>
 
-                <div className="w-15 h-10 bg-violet-500 rounded-full p-1 flex items-center mb-6 absolute top-3 right-3">
-                    <div className="w-5 h-5 bg-white rounded-full ml-auto"></div>
+                <div className="w-10 h-8 bg-violet-500 rounded-full p-1 flex items-center mb-6 absolute top-3 right-3">
+                    <div className="w-4 h-4 bg-white rounded-full ml-auto"></div>
+                </div>
+            </div>
+
+
+            <div className='bg-white p-6 mt-6 shadow-2xl  rounded-2xl grid-cols-2'>
+                <h1 className='font-bold'>User</h1>
+                <div className='flex gap-3 mt-3'>
+                    <div className='h-10  w-10 align-center text-2xl text-violet-500  border-2 rounded-full flex justify-center'>U1</div>
+                <div className='h-10  w-10 align-center text-2xl text-violet-500  border-2 rounded-full flex justify-center'>U1</div>
+                <div className='h-10  w-10  align-center text-2xl text-violet-500 border-2 rounded-full flex justify-center'>U1</div> <span className='text-1xl mt-2'>+</span>
                 </div>
             </div>
         </div>
